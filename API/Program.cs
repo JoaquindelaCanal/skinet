aeb26d35692551e72eb31e2aa485b5503ca08884
JoaquindelaCanal <joaquindelacanal@hotmail.com>
2024-10-30 21:34:06 -0300

    Get Product

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Core.Interfaces;
+
 using Infrastructure.Data;
 
 using Microsoft.EntityFrameworkCore;
@@ -14,10 +16,27 @@ builder.Services.AddDbContext<StoreContext>(options =>
 });
 
 
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 var app = builder.Build();
 
 app.MapControllers();
+
+try
+{
+    using var scope = app.Services.CreateScope();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+    throw;
+}
 
 app.Run();
