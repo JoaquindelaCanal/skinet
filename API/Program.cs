21f2ddda0f03c8cb33c333d92de9f1dcd344684d
JoaquindelaCanal <joaquindelacanal@hotmail.com>
2024-12-09 20:43:59 -0300

    Shopping Cart

--- API/Program.cs
+++ API/Program.cs
@@ -4,9 +4,12 @@ using Core.Interfaces;
 
 using Infrastructure;
 using Infrastructure.Data;
+using Infrastructure.Services;
 
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.EntityFrameworkCore;
+
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +22,16 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
+{
+    var connString = builder.Configuration.GetConnectionString("Redis")
+        ?? throw new Exception("Cannot get redis connection string");
+    var configuration = ConfigurationOptions.Parse(connString, true);
+    return ConnectionMultiplexer.Connect(configuration);
+});
+
+builder.Services.AddSingleton<ICartService, CartService>();
+
 builder.Services.AddCors();
 
 builder.Services.AddProblemDetails(options =>
